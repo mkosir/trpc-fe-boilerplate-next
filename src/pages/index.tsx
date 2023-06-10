8f24b6a7b66784a84de3e394b64fd48ab022d568
mkosir <kosir.mar@gmail.com>
2023-06-10 17:15:27 +0200

    chore(deps): update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <Head>
         <title>tRPC Frontend Boilerplate</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
-      <img src="/heisenberg.png" alt="heisenberg" width={85} />
+      <img src="./heisenberg.png" alt="heisenberg" width={85} />
       <div>tRPC Frontend Boilerplate</div>
     </>
   );
