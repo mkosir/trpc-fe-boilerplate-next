9be5565baab326b54df6a8b088cd78f40ad85329
mkosir <kosir.mar@gmail.com>
2023-06-10 14:14:45 +0000

    Deploying to gh-pages from @ mkosir/trpc-fe-boilerplate@f49273422f13d0e29547d1d10ab33b9e18464d97 🚀

---
+++ _next/static/MDCbuptRScp2ZP7AEMRvv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8e23e2be85300958.css","static/chunks/pages/index-aae8c37df1623a9b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
