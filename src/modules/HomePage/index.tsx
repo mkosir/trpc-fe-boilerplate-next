c39ab44d540fd5e2dad3058cf2a280acf7265e3a
mkosir <kosir.mar@gmail.com>
2023-06-11 11:59:42 +0200

    chore(deps): update dependencies

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,8 +1,19 @@
+import { trpcApiBoilerplateClient } from 'common/trpc-api-boilerplate';
+
 export const HomePage = () => {
+  const seedDbMutation = trpcApiBoilerplateClient.util.seedDb.useMutation({
+    onSuccess: (data) => console.log(data),
+  });
+
+  const handleSeedDb = () => {
+    seedDbMutation.mutate();
+  };
+
   return (
     <div>
       <img src="./heisenberg.png" alt="heisenberg" width={120} />
       <div>tRPC Frontend Boilerplate</div>
+      <button onClick={handleSeedDb}>Seed DB</button>
     </div>
   );
 };
