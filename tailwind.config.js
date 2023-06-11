0e0ee8031528549cc0c09800bb847b3d069833b8
mkosir <kosir.mar@gmail.com>
2023-06-11 12:51:42 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        blueSky: {
+          100: '#64b0ce',
+          150: '#3d9cc2',
+        },
+      },
+    },
+  },
   plugins: [],
 };
