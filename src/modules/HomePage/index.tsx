ed047c5df1e8464e7b46a0057c9a302d7af275e3
mkosir <kosir.mar@gmail.com>
2023-06-12 14:21:15 +0200

    chore(deps): update dependencies

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -17,7 +17,11 @@ export const HomePage = () => {
   return (
     <div className="flex justify-center text-center mt-10">
       <div>
-        <img src="heisenberg.png" alt="heisenberg" className="mx-auto w-[110px] mb-1" />
+        <img
+          src="https://raw.githubusercontent.com/mkosir/trpc-fe-boilerplate/main/misc/heisenberg.png"
+          alt="heisenberg"
+          className="mx-auto w-[110px] mb-1"
+        />
         <div className="mb-1 text-lg font-medium">tRPC Frontend Boilerplate</div>
         <button
           className="bg-blueSky-400 hover:bg-blueSky-500 active:bg-blueSky-600 text-sm text-white p-1 px-3 rounded"
